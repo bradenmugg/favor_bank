ea6f6acf18f0777de2b822e4267d4e458d04fd30
bradenmugg <braden.mugg@gmail.com>
2015-07-10 04:29:14 +0000

    Sessions and Cookies

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -57,4 +57,9 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
   
+  test "authenticated? should return false for a user with nil digest" do
+    return false if remember_digest.nil?
+    assert_not @user.authenticated?('')
+  end
+  
 end
