bd740b9bad52071d4885ae1d94c01f1550134adb
bradenmugg <braden.mugg@gmail.com>
2015-07-12 03:46:26 +0000

    Password Resets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'static_pages#home'
   get 'about'   => 'static_pages#about'
   get 'signup'  => 'users#new'
@@ -8,5 +12,6 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
 end
